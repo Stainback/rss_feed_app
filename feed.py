6c36ef02b5f2a21e4c5a0f8501b44d34cc1fc5de
ANMI <migunov1au@gmail.com>
2024-07-23 17:12:52 +0300

    removed EntryHandler class

--- feed.py
+++ feed.py
@@ -1,17 +1,13 @@
 import feedparser
 
 
-class EntryHandler:
-    def __init__(self, data):
-        self.title = data.title
-        self.description = data.description
-        self.link = data.link
-
-
 class Feed:
     def __init__(self, url):
         self.url = url
         self.feed = feedparser.parse(self.url)
 
-    def __getitem__(self, index: int) -> EntryHandler:
-        return EntryHandler(self.feed['entries'][index])
+    def __repr__(self):
+        return self.feed.get("title", "No Title")
+
+    def __getitem__(self, index: int):
+        return self.feed['entries'][index]
